7b2f0a365481b1bf0b62d572ee0f9f9a05769a9f
serene <e1355867@u.nus.edu>
2025-08-29 23:09:44 +0800

    Update String by to LocalDate type

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,15 +1,22 @@
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 /**
  * Represents a deadline task with a description and a due date.
  */
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDate by;
 
+    /**
+     * Creates a Deadline task.
+     *
+     * @param description Description of the task.
+     * @param by Deadline in yyyy-mm-dd format.
+     */
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = LocalDate.parse(by);
     }
 
     @Override
@@ -19,6 +26,7 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: "
+                + by.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")";
     }
 }
