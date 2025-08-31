abb9eb309ec9af7bc21fe12101e8abba2d0e0f7c
serene <e1355867@u.nus.edu>
2025-08-31 15:39:50 +0800

    Add and update Parser class to deal with user commands

---
+++ src/main/java/Parser.java
@@ -0,0 +1,19 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+/**
+ * Parser is responsible for parsing user input commands and extracting details.
+ */
+public class Parser {
+    private static final DateTimeFormatter FILE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+
+    /** Parses string to LocalDateTime */
+    public static LocalDateTime parseDateTime(String input) throws CapyException {
+        try {
+            return LocalDateTime.parse(input, FILE_FORMAT);
+        } catch (DateTimeParseException e) {
+            throw new CapyException("OOPS! Invalid date/time format. Use yyyy-MM-dd HHmm");
+        }
+    }
+}
