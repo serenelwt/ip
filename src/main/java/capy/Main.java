03c91ffbdd6f5b9cd81286016c06ec09754f3016
serene <e1355867@u.nus.edu>
2025-09-04 14:05:19 +0800

    Style the controls for capy

--- src/main/java/capy/Main.java
+++ src/main/java/capy/Main.java
@@ -1,5 +1,6 @@
 package capy;
 
+import javafx.scene.layout.Region;
 import javafx.application.Application;
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -44,6 +45,35 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.show();
 
+        //Formatting the window to look as expected
+
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput, 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
         //More code to be added here later
     }
 }
