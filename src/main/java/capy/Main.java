de34abcda7d12b00db8fbc1661e2b2d527fada59
serene <e1355867@u.nus.edu>
2025-09-04 21:29:48 +0800

    Set limit to window size

--- src/main/java/capy/Main.java
+++ src/main/java/capy/Main.java
@@ -22,6 +22,8 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setMinHeight(220);
+            stage.setMinWidth(417);
             fxmlLoader.<capy.MainWindow>getController().setCapy(capy);  // inject the Duke instance
             stage.show();
         } catch (IOException e) {
